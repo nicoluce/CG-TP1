1aee5571328473d6cedd2f3ead1a71a9d705a26c
Nicolas Luce <nicolas.luce@urbansim.com>
2021-04-05 17:15:37 -0300

    Lint

--- ejercicio.js
+++ ejercicio.js
@@ -1,13 +1,22 @@
 // La imagen que tienen que modificar viene en el parámetro image y contiene inicialmente los datos originales
 // es objeto del tipo ImageData ( más info acá https://mzl.la/3rETTC6  )
 // Factor indica la cantidad de intensidades permitidas (sin contar el 0)
-function dither(image, factor)
-{
-    // completar
+function dither(image, factor) {
+  /**
+     * Pseudo code
+     * 
+     * for each y from top to bottom do
+        for each x from left to right do
+            oldpixel := pixel[x][y]
+            newpixel := find_closest_palette_color(oldpixel)
+            pixel[x][y] := newpixel
+            quant_error := oldpixel - newpixel
+            pixel[x + 1][y    ] := pixel[x + 1][y    ] + quant_error × 7 / 16
+            pixel[x - 1][y + 1] := pixel[x - 1][y + 1] + quant_error × 3 / 16
+            pixel[x    ][y + 1] := pixel[x    ][y + 1] + quant_error × 5 / 16
+            pixel[x + 1][y + 1] := pixel[x + 1][y + 1] + quant_error × 1 / 16
+     */
 }
 
 // Imágenes a restar (imageA y imageB) y el retorno en result
-function substraction(imageA,imageB,result) 
-{
-    // completar
-}
+function substraction(imageA, imageB, result) {}
